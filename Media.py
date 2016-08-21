5a5251e13578ecbf2f293e5dad9455e17d9d3c82
anshikagarg <anshikagarg2689@gmail.com>
2016-08-21 17:27:33 -0400

    Update Media.py

--- Media.py
+++ Media.py
@@ -1,7 +1,13 @@
 class Movie():
-	def __init__(self, movie_title, movie_description, movie_poster, movie_trailer):
-		self.title = movie_title
-		self.description = movie_description
-		self.poster = movie_poster
-		self.trailer = movie_trailer
+    """This class helps define the Model for Movie.
+       Movie class has 4 values which define the
+       name of the movie(title), the description of
+       the movie, it provides a image of the poster &
+       a youtube link to the trailer."""
 
+    def __init__(self, movie_title, movie_description,
+                 movie_poster, movie_trailer):
+                self.title = movie_title
+                self.description = movie_description
+                self.poster = movie_poster
+                self.trailer = movie_trailer
